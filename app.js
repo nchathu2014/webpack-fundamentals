e6c62b98bd15ffb6c5aa48913fa059f46c2089c5
Chathuranga <UCHATNU@PEROOT.COM>
2016-07-01 21:46:19 +0530

    add webpack config file to the project

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
  * Created by UCHATNU on 7/1/2016.
  */
 
-var disPlayMessage = "<h1>Hello World from Webpack</h1>";
+var disPlayMessage = "<h1>Hello World from Webpack!!!!!!!</h1>";
 var consoleMessage = console.log("I am a console message");
 
 document.getElementById('app').innerHTML = disPlayMessage;
