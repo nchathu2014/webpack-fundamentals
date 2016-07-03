fcad95122f46e7f0eee9fcd9c85520a31a950e47
Chathuranga <UCHATNU@PEROOT.COM>
2016-07-03 16:00:25 +0530

    add images to the project

--- src/main.js
+++ src/main.js
@@ -2,12 +2,17 @@
  * Created by UCHATNU on 7/3/2016.
  */
 
+let img = document.createElement('img');
+img.src=require("./img/capture.png");
+document.getElementById('img-container').appendChild(img);
+
 require('./css/bootstrap.min.css');
 require('./scss/common.scss');
 require('./less/main.less');
 require('./css/custom.css');
 
 function loadingApp(){
+
     let domElement = document.getElementById('app');
     let element = "<p class=\"navbar navbar-text navbar-right\">Signed in as <a href=\"#\" class=\"navbar-link\">Mark Otto</a></p>" +
         "<div class=\"panel panel-default\"> " +
--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,7 @@ module.exports ={
             {test: /\.(woff|woff2)$/, loader:"url?prefix=font/&limit=5000" },
             {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
             {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml" },
+            {test:/\.(png|jpg)$/,exclude:/node_modules/,loader:"url-loader?limit=1000"}
         ]
     },
 
