36012821ffde04ce49b6b970d5e3d39498b6c96b
Chathuranga <UCHATNU@PEROOT.COM>
2016-07-03 19:40:25 +0530

    add fonts to project

--- webpack.config.js
+++ webpack.config.js
@@ -24,11 +24,9 @@ module.exports ={
             {test:/\.css$/,exclude:/node_modules/,loader:"style-loader!css-loader"},
             {test:/\.scss$/,exclude:/node_modules/,loader:"style-loader!css-loader!autoprefixer-loader!sass-loader"},
             {test:/\.less$/,exclude:/node_modules/,loader:"style-loader!css-loader!autoprefixer-loader!less-loader"},
-            {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
             {test: /\.(woff|woff2)$/, loader:"url?prefix=font/&limit=5000" },
-            {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
             {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml" },
-            {test:/\.(png|jpg)$/,exclude:/node_modules/,loader:"url-loader?limit=1000"}
+            {test:/\.(png|jpg|eot|ttf)$/,exclude:/node_modules/,loader:"url-loader?limit=1000"}
         ]
     },
 
